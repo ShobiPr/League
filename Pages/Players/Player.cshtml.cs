506e66c9bc2c7498b5716cb7dd32c94cfb08807e
Shobiha Premkumar <sho.pre08@gmail.com>
2020-09-07 13:39:39 +0200

    players and player detail page added

---
+++ Pages/Players/Player.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using League.Models;
+using League.Data;
+
+namespace League.Pages.Players
+{
+    public class PlayerModel : PageModel
+    {
+        private readonly LeagueContext _context;
+
+        public PlayerModel(LeagueContext context)
+        {
+            _context = context;
+        }
+
+        public Player Player { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Player = await _context.Players.FirstOrDefaultAsync();
+        }
+
+
+    }
+}
